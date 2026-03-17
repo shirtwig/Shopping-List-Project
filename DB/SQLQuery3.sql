926569fb86c6ef61985c65ebd7b01ab99c2f0b62
Shir Twig <shirtwig2@gmail.com>
2026-03-17 14:38:15 +0200

    Update SQLQuery3.sql

--- DB/SQLQuery3.sql
+++ DB/SQLQuery3.sql
@@ -1,16 +1,19 @@
-INSERT INTO Categories (Name,DisplayOrder) VALUES ('🥦 ירקות ופירות',1);
-INSERT INTO Categories (Name,DisplayOrder) VALUES ('🧀 מוצרי חלב ',2);
-INSERT INTO Categories (Name,DisplayOrder) VALUES ('🥐 מאפים ',3);
-INSERT INTO Categories (Name,DisplayOrder) VALUES ('🍫 מתוקים ',4);
-INSERT INTO Categories (Name,DisplayOrder) VALUES ('🥨 חטיפים ומלוחים ',5);
-INSERT INTO Categories (Name,DisplayOrder) VALUES ('🧼 חומרי ניקוי ',6);
+USE [ShoppingListDB];
+GO
 
+INSERT INTO Categories (Name, DisplayOrder) VALUES 
+(N'🥦 ירקות ופירות', 1),
+(N'🧀 מוצרי חלב', 2),
+(N'🥐 מאפים', 3),
+(N'🍫 מתוקים', 4),
+(N'🥨 חטיפים ומלוחים', 5),
+(N'🧼 חומרי ניקוי', 6);
 
-INSERT INTO Statuses (Name,ColorHex) VALUES('🛒 נקנה כבר!','#B9FBC0');
-INSERT INTO Statuses (Name,ColorHex) VALUES('🙈 אופס, תבדקו שוב אם יש בבית','#FFF5B7');
-INSERT INTO Statuses (Name,ColorHex) VALUES('✨ דחוף ביותר!','#FFCFD2');
+INSERT INTO Statuses (Name, ColorHex) VALUES
+(N'🛒 נקנה כבר!', '#B9FBC0'),
+(N'🙈 אופס, תבדקו שוב אם יש בבית', '#FFF5B7'),
+(N'✨ דחוף ביותר!', '#FFCFD2');
 
-INSERT INTO Units (Name) VALUES('ק"ג');
-INSERT INTO Units (Name) VALUES('ליטר');
-INSERT INTO Units (Name) VALUES('יחידה');
-INSERT INTO Units (Name) VALUES('חבילה');
+INSERT INTO Units (Name) VALUES
+(N'ק"ג'), (N'ליטר'), (N'יחידה'), (N'חבילה');
+GO
